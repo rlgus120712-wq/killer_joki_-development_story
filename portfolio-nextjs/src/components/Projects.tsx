7a6a1526a4c6773600e2874e5a0dea7854cd328a
rlgus120712-wq <rlgus120712@gmail.com>
2025-10-22 15:23:31 +0900

    Projects 컴포넌트에 ref={ref} 추가하여 애니메이션이 제대로 작동하도록 수정

--- portfolio-nextjs/src/components/Projects.tsx
+++ portfolio-nextjs/src/components/Projects.tsx
@@ -26,7 +26,7 @@ const Projects = () => {
   });
 
   return (
-    <section id="projects" className="py-12 sm:py-20 px-3 sm:px-6">
+    <section id="projects" className="py-12 sm:py-20 px-3 sm:px-6" ref={ref}>
       <div className="max-w-6xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
